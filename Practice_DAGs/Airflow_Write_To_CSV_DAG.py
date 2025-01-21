ba409d0a1873376f26672c496f6ce7b8545012da
Pepijn Schouten <pepijn.sch@gmail.com>
2025-01-22 00:05:54 +0100

    add write to csv dag

---
+++ Practice_DAGs/Airflow_Write_To_CSV_DAG.py
@@ -0,0 +1,50 @@
+import csv
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# function to write to csv
+def write_csv():
+    
+    data = [
+        ["Name", "Age", "City"],
+        ["John", 25, "New York"],
+        ["Jane", 30, "San Francisco"],
+        ["Bob", 35, "Seattle"],
+        ["Alice", 28, "Los Angeles"],
+    ]
+    
+    file_path = "/tmp/to_csv_dag.csv"
+    
+    with open(file_path, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+        
+    print(f"csv saved to {file_path}")
+    
+
+# default arguments for DAG
+default_args = {
+    'owner': 'pepijnschouten',
+    'depends_on_past': False,
+    'retries': 0,
+}
+
+# instantiate DAG
+with DAG(
+    "write_csv_dag",
+    default_args=default_args,
+    description="A simple CSV write DAG",
+    schedule=None,  # Manual trigger
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+) as dag:
+    
+    # define the task
+    write_csv_task = PythonOperator(
+        task_id='write_csv_task',
+        python_callable=write_csv,
+    )
+    
+# set task dependencies
+write_csv_task
